b82027ec156c96d608f9bf9e58c552cdb1027aa7
Partho P. Das <this@parthopdas.com>
2016-10-27 12:07:26 +1100

    Step 3. Implement Traversable interfaces for List type (Applicative flavor)

---
+++ App/List.fs
@@ -0,0 +1,29 @@
+﻿module App.List
+
+let traverseAsyncA f list = 
+    let (<*>) = Async.apply
+    let retn = Async.retn
+
+    let cons head tail = head :: tail
+    let initState = retn []
+
+    let folder head tail = 
+        retn cons <*> (f head) <*> tail
+
+    List.foldBack folder list initState
+
+let sequenceAsyncA x = traverseAsyncA id x
+
+let traverseResultA f list = 
+    let (<*>) = Result.apply
+    let retn = Result.Success
+
+    let cons head tail = head :: tail
+    let initState = retn []
+
+    let folder head tail = 
+        retn cons <*> (f head) <*> tail
+
+    List.foldBack folder list initState
+
+let sequenceResultA x = traverseResultA id x
