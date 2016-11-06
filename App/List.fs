84f275d9fba5fcba8a2af4ce6028dfdcb34276db
Partho P. Das <this@parthopdas.com>
2016-11-06 15:45:05 +0700

    Step 5. Implement Traversable interfaces for List type (Monadic flavor)

--- App/List.fs
+++ App/List.fs
@@ -14,6 +14,22 @@ let traverseAsyncA f list =
 
 let sequenceAsyncA x = traverseAsyncA id x
 
+let traverseAsyncM f list =
+    let (>>=) x f = Async.bind f x
+    let retn = Async.retn
+
+    let cons head tail = head :: tail
+
+    let initState = retn []
+    let folder head tail = 
+        f head >>= (fun h -> 
+        tail >>= (fun t ->
+        retn (cons h t) ))
+
+    List.foldBack folder list initState
+
+let sequenceAsyncM x = traverseAsyncM id x
+
 let traverseResultA f list = 
     let (<*>) = Result.apply
     let retn = Result.Success
@@ -27,3 +43,19 @@ let traverseResultA f list =
     List.foldBack folder list initState
 
 let sequenceResultA x = traverseResultA id x
+
+let traverseResultM f list =
+    let (>>=) x f = Result.bind f x
+    let retn = Result.Success
+
+    let cons head tail = head :: tail
+
+    let initState = retn []
+    let folder head tail = 
+        f head >>= (fun h -> 
+        tail >>= (fun t ->
+        retn (cons h t) ))
+
+    List.foldBack folder list initState
+
+let sequenceResultM x = traverseResultM id x
